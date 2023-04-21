0755e1306bb9821d75cc63c95f56ad8e439761be
Xianhan Qin <q764579066@gmail.com>
2023-04-20 23:15:57 -0400

    result for midterm

--- src/concreteNet.py
+++ src/concreteNet.py
@@ -2,9 +2,8 @@ import torch
 from torch import nn
 
 class ConcreteAutoencoder(nn.Module):
-    def __init__(self, input_dim, k, hidden_dim,device, temperature=0.1):
+    def __init__(self, input_dim, k, hidden_dim,device):
         super().__init__()
-        self.temperature = temperature
         self.device = device
         self.alpha = nn.Parameter(torch.rand(input_dim, k))
         self.decoder = nn.Sequential(
@@ -17,21 +16,14 @@ class ConcreteAutoencoder(nn.Module):
         U = torch.rand(shape)
         return -torch.log(-torch.log(U + eps) + eps)
 
-    def gumbel_softmax_sample(self, alpha, temperature):
+    def gumbel_softmax(self, alpha, temperature):
         y = alpha + self.sample_gumbel(alpha.size()).to(self.device)
         return nn.functional.softmax(y / temperature, dim = 0)
-
-    def gumbel_softmax(self, alpha, temperature):
-        y = self.gumbel_softmax_sample(alpha, temperature)
-        shape = y.size()
-        _, ind = y.max(dim=-1)
-        y_hard = torch.zeros_like(y).view(-1, shape[-1])
-        y_hard.scatter_(1, ind.view(-1, 1), 1)
-        y_hard = y_hard.view(*shape)
-        return (y_hard - y).detach() + y
-
-    def forward(self, x):
-        m = self.gumbel_softmax_sample(self.alpha, self.temperature)
+    
+    def forward(self, x, temperature):
+        m = self.gumbel_softmax(self.alpha, temperature)
         z = torch.mm(x,m)
         reconstruction = self.decoder(z)
         return reconstruction, z, m
+
+
--- src/trainer.py
+++ src/trainer.py
@@ -1,4 +1,22 @@
 import torch 
-import torch.nn as nn
+import time 
 
-
+def train(net, loss, dataloader, num_epoch, learning_rate, weight_decay, device, init_temp= 10, end_temp= 0.1):
+    net = net.to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr = learning_rate, weight_decay=weight_decay)
+    start_time  = time.time()
+    net.train()
+    for epoch in range(1, num_epoch+1):
+        train_loss = 0
+        temp = init_temp * (end_temp/init_temp)**(epoch/num_epoch)
+        for data in dataloader:
+            input = data.to(device, non_blocking = True)
+            optimizer.zero_grad()
+            reconstruction, _, _ = net(input, temp)
+            l = loss(reconstruction, input)
+            l.backward()
+            optimizer.step()
+            train_loss += l
+        process_time = time.time() - start_time
+        train_loss /= len(dataloader)
+        print("Epoch: %d ; Loss: %.5f; Temperature: %.3f; Time: %.2f s" %(epoch, train_loss,temp, process_time))
